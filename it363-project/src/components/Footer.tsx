23646ec02619913d835299abc06c70d52c372347
Grant Barkalow <Gbarkalow302@gmail.com>
2025-11-17 16:36:56 -0600

    edited Footer

--- it363-project/src/components/Footer.tsx
+++ it363-project/src/components/Footer.tsx
@@ -6,6 +6,8 @@ import { usePathname } from "next/navigation";
 const footerLinks = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About Us" },
+  { href: "/menupage", label: "Menu"},
+  { href: "/book", label: "Book with us", className: "text-lg font-semibold rounded-full bg-red-800 text-white px-5 py-2.5"},
 ];
 
 export default function Footer() {
@@ -34,7 +36,7 @@ export default function Footer() {
           <ul className="flex flex-wrap items-center justify-center gap-6 text-sm">
             {footerLinks.map((link) => (
               <li key={link.href}>
-                <Link className="transition hover:text-neutral-900" href={link.href}>
+                <Link href={link.href} className={`transition hover:text-neutral-900 ${link.className || ""}`} >
                   {link.label}
                 </Link>
               </li>
@@ -46,7 +48,11 @@ export default function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Facebook
+                <img
+              src="/Facebook_logo_(square).png"
+              alt="Facebook"
+              className="h-12 w-12 rounded-md object-contain hover:ring-2 hover:ring-white/90 focus-visible:ring-2 focus-visible:ring-white/90 active:scale-90 transition-transform duration-150"
+            />
               </a>
             </li>
           </ul>
